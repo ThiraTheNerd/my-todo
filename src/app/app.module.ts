1274d14646e2eb409ae68055a6400765d6d678ed
ThiraTheNerd <thiragithinji@gmail.com>
2021-05-17 21:04:03 +0300

    add all the changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule
